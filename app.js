20a694d4d160c2491a0e4fa343c7902ca613e6fd
jazzari <passionjazz@gmail.com>
2021-06-07 16:46:43 +0200

    Create campground model

--- app.js
+++ app.js
@@ -2,21 +2,26 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/farmStand', {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => {
-    console.log('MONGO CONNECTION OPEN!!!!');
-})
-.catch(err => {
-    console.log('OH NOO!, MONGO CONNECTION ERROR!!!');
-    console.log(err);
-})
+const CampGround = require('./models/campground');
+mongoose.connect('mongodb://localhost:27017/pitch-camp', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "connection error:"));
+db.once('open', () => {
+    console.log('Database connected');
+});
 const methodOverride = require('method-override');
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home');
+})
+app.get('/makecampground', async (req, res) => {
+    const camp = new CampGround({title: 'My Backyard', description: 'cheap camping!'});
+    await camp.save();
+    res.send(camp);
 })
 
 app.listen(3000, () => {
