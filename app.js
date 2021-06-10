e091ad641201b78af76a43be93b44fbc95240c35
jazzari <passionjazz@gmail.com>
2021-06-10 14:29:22 +0200

    Add edit and update action to campgrounds

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const CampGround = require('./models/campground');
+const methodOverride = require('method-override');
 mongoose.connect('mongodb://localhost:27017/pitch-camp', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, "connection error:"));
 db.once('open', () => {
     console.log('Database connected');
 });
-const methodOverride = require('method-override');
-
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -36,7 +36,15 @@ app.get('/campgrounds/:id', async (req, res) => {
     const campground = await CampGround.findById(req.params.id);
     res.render('campgrounds/show', { campground });
 });
-
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await CampGround.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+})
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await CampGround.findByIdAndUpdate(id, { ...req.body.campground }, { new: true });
+    res.render('campgrounds/show', { campground });
+})
 
 app.listen(3000, () => {
     console.log('Serving on Port 3000');
