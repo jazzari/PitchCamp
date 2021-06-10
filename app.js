beb258879fd9ce05236391e9ed0d82425a990540
jazzari <passionjazz@gmail.com>
2021-06-10 11:04:41 +0200

    Add create action and new views to campgrounds

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const methodOverride = require('method-override');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -22,10 +24,19 @@ app.get('/campgrounds', async (req, res) => {
     const campgrounds = await CampGround.find({});
     res.render('campgrounds/index', { campgrounds });
 });
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+app.post('/campgrounds', async (req, res) => {
+    const campground = new CampGround(req.body.campground);
+    await campground.save();
+    res.redirect(`campgrounds/${campground._id}`);
+})
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await CampGround.findById(req.params.id);
     res.render('campgrounds/show', { campground });
 });
+
 
 app.listen(3000, () => {
     console.log('Serving on Port 3000');
