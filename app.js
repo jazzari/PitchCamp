a4e25f7b4f028c1e316f9d908c0c40ff6ee0d128
jazzari <passionjazz@gmail.com>
2021-06-10 10:37:29 +0200

    Add campgrounds index and show views

--- app.js
+++ app.js
@@ -17,12 +17,15 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home');
-})
-app.get('/makecampground', async (req, res) => {
-    const camp = new CampGround({title: 'My Backyard', description: 'cheap camping!'});
-    await camp.save();
-    res.send(camp);
-})
+});
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await CampGround.find({});
+    res.render('campgrounds/index', { campgrounds });
+});
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await CampGround.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
+});
 
 app.listen(3000, () => {
     console.log('Serving on Port 3000');
