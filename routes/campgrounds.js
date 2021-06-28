90fab2ffe9f98e167febb4efbb1dbbd8c84743e7
jazzari <passionjazz@gmail.com>
2021-06-28 16:28:49 +0200

    Add a flash-error partial

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -30,10 +30,18 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
 }));
 router.get('/:id', catchAsync(async (req, res) => {
     const campground = await CampGround.findById(req.params.id).populate('reviews');
+    if(!campground) {
+        req.flash('error', 'Cannot find that Campground!');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/show', { campground, title: campground.title });
 }));
 router.get('/:id/edit', catchAsync(async (req, res) => {
     const campground = await CampGround.findById(req.params.id);
+    if(!campground) {
+        req.flash('error', 'Cannot find that Campground!');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/edit', { campground, title: campground.title });
 }));
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
