0b421d0d8631ca2d3f25a32e0b5947c8f36bc73d
jazzari <passionjazz@gmail.com>
2021-06-30 16:10:11 +0200

    Add returnTo behavior when user is not logged in

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLoggedIn = (req, res, next) => {
+    req.session.returnTo = req.originalUrl;
     if(!req.isAuthenticated()){
         req.flash('error', 'You must be sign in first!');
         return res.redirect('/login');
--- routes/users.js
+++ routes/users.js
@@ -28,7 +28,9 @@ router.get('/login', (req, res) => {
 })
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), async (req, res) => {
     req.flash('success', 'Welcome back!');
-    res.redirect('/campgrounds');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
 })
 router.get('/logout', (req, res) => {
   req.logout();
