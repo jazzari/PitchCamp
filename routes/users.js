0bcbced77ae0143f1ccce4d70d8904f0871bb854
jazzari <passionjazz@gmail.com>
2021-06-30 15:57:47 +0200

    Add loggedIn to new registered users

--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,16 @@ const passport = require('passport');
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
-router.post("/register", catchAsync(async (req, res) => {
+router.post("/register", catchAsync(async (req, res, next) => {
     try {
       const { email, username, password } = req.body;
       const user = new User({ username, email });
       const registeredUser = await User.register(user, password);
-      req.flash("success", "Welcome to PitchCamp!");
-      res.redirect("/campgrounds");
+      req.login(registeredUser, err => {
+        if(err) return next(err);
+        req.flash("success", "Welcome to PitchCamp!");
+        res.redirect("/campgrounds");
+      });
     } catch (e) {
       req.flash("error", e.message);
       res.redirect("/register");
