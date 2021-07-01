f4711037197f3a4fcab66e95366604ce0c2acf65
jazzari <passionjazz@gmail.com>
2021-07-01 09:05:35 +0200

    Add an author to campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -25,12 +25,13 @@ router.get('/new', isLoggedIn, (req, res) => {
 });
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     const campground = new CampGround(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash('success', 'Successfully made a new Campground!');
     res.redirect(`campgrounds/${campground._id}`);
 }));
 router.get('/:id', catchAsync(async (req, res) => {
-    const campground = await CampGround.findById(req.params.id).populate('reviews');
+    const campground = await (await CampGround.findById(req.params.id).populate('reviews').populate('author'));
     if(!campground) {
         req.flash('error', 'Cannot find that Campground!');
         return res.redirect('/campgrounds');
