1dcf854c1505fc57edbcdf2b2fa6be219e5f1e6c
jazzari <passionjazz@gmail.com>
2021-07-12 19:12:35 +0200

    Add custom popup to map's markers

--- models/campground.js
+++ models/campground.js
@@ -10,6 +10,8 @@ ImageSchema.virtual('thumbnail').get(function() {
     return this.url.replace('/upload', '/upload/w_200');
 })
 
+const opts = { toJSON: { virtuals: true } };
+
 const campgroundSchema = new Schema({
     title: String,
     images: [ImageSchema],
@@ -37,7 +39,13 @@ const campgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
-});
+}, opts);
+
+campgroundSchema.virtual('properties.popUpMarkup').get(function() {
+    return `
+        <strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+        <p>${this.description.substring(0, 20)}...</p>`;
+})
 
 campgroundSchema.post('findOneAndDelete', async function(doc) {
     if(doc) {
