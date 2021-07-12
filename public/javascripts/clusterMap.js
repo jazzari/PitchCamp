7ff5fced42002f9b5262cfdcf37c3238dffd8392
jazzari <passionjazz@gmail.com>
2021-07-12 18:06:51 +0200

    Setting up basic clustering

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,6 +1,6 @@
 
 mapboxgl.accessToken = mapToken;
-var map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/mapbox/dark-v10',
 center: [-103.59179687498357, 40.66995747013945],
@@ -15,7 +15,7 @@ map.addSource('earthquakes', {
 type: 'geojson',
 // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
 // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-data: 'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+data: campgrounds,
 cluster: true,
 clusterMaxZoom: 14, // Max zoom to cluster points on
 clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
