7acf4b861efbf3b7f306c1f252375a87aff94832
jazzari <passionjazz@gmail.com>
2021-07-12 17:40:11 +0200

    Fix seeds with valid campground's coordinates

--- seeds/index.js
+++ seeds/index.js
@@ -27,7 +27,10 @@ const seedDB = async () => {
             price, 
             geometry: { 
                 type: 'Point', 
-                coordinates: [ -0.3333, 39.4167 ] 
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude
+                ] 
             },
             images: [
                 {
