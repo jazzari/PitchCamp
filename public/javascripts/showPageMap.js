007e10c67c7c46adec8bf08b5137f217134eb35c
jazzari <passionjazz@gmail.com>
2021-07-12 16:10:30 +0200

    Center map on campground

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -3,7 +3,10 @@
   const map = new mapboxgl.Map({
   container: 'map', // container ID
   style: 'mapbox://styles/mapbox/streets-v11', // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9 // starting zoom
+  center: campground, // starting position [lng, lat]
+  zoom: 10 // starting zoom
   });
 
+  new mapboxgl.Marker()
+  .setLngLat(campground)
+  .addTo(map)
