90183963f7f77341e984de4ed436acdf26bd43ee
jazzari <passionjazz@gmail.com>
2021-07-12 16:58:42 +0200

    Add popup to map's marker

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,11 +2,17 @@
   mapboxgl.accessToken = mapToken;
   const map = new mapboxgl.Map({
   container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/streets-v11', // style URL
-  center: campground, // starting position [lng, lat]
+  style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
+  center: campground.geometry.coordinates, // starting position [lng, lat]
   zoom: 10 // starting zoom
   });
 
   new mapboxgl.Marker()
-  .setLngLat(campground)
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({offset: 15})
+    .setHTML(
+      `<h4>${campground.title}</h4><p>${campground.location}</p>`
+    )
+  )
   .addTo(map)
